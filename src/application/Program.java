7a11a74a5085e5cfd138851d26d17c8a7de644e1
Levi Martines <levi.mferreira@gmail.com>
2019-04-18 13:46:13 -0300

    jbdc2, acesso a tabelas com executeQuery salvando em um ResultSEt. Acesso as dados com get.Fechamento de recursos com métodos auxiliares.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
-
 	public static void main(String[] args) {
-	/*	dar comando SET @@global.time_zone = '+3:00'
-		caso de erro de fuso-horário	*/
+
+		Connection conn = null;
+		//Statemente = intarface usada para executar SQL
+		Statement st = null;
+		//tabela resultado
+		ResultSet rs = null;
+
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();	
+
+			rs = st.executeQuery("SELECT * FROM department");
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatemente(st);
+			DB.closeConnection();
+		}
 		
-		Connection conn = DB.getConnection();	
-		
-		DB.closeConnection();
 	}
 }
